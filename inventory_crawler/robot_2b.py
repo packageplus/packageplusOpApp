8971a27f04dd88fa42e6c39fa957ce0132e3467f
配客嘉 <139420301+packageplus@users.noreply.github.com>
2025-06-27 11:19:37 +0800

    Update robot_2b.py

--- inventory_crawler/robot_2b.py
+++ inventory_crawler/robot_2b.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options 
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import re
 from datetime import datetime
