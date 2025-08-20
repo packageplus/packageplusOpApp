0abd3b936444f72899163ce3ce40c06ce4edd623
配客嘉 <139420301+packageplus@users.noreply.github.com>
2025-08-20 22:44:42 +0800

    Update robot_2b.py

--- inventory_crawler/robot_2b.py
+++ inventory_crawler/robot_2b.py
@@ -161,7 +161,7 @@ while True:
     try:
         # next_a = driver.find_element(By.CSS_SELECTOR, "li.paginate_button.next:not(.disabled) > a")
         next_a = driver.find_element(By.CSS_SELECTOR, ".btn.btn-success:not(.disabled)")
-        print("找到下一頁按鈕了")
+        print("✅ 找到下一頁按鈕了")
     except NoSuchElementException:
         print("❌ 沒有找到下一頁按鈕，停止翻頁。")
         break
@@ -179,9 +179,9 @@ while True:
             # lambda d: d.find_elements(By.CSS_SELECTOR, "#stock-table tbody tr")
                         # and d.find_elements(By.CSS_SELECTOR, "#stock-table tbody tr")[0].text != first_row_text
         )
-        testDefine = driver.find_elements(By.CSS_SELECTOR, ".rdt_TableBody .rdt_TableRow")[0].find_elements(By.CSS_SELECTOR, ".rdt_TableCell")[1].text
+        testDefine = driver.find_elements(By.CSS_SELECTOR, ".rdt_TableBody .rdt_TableRow")[0].text
         print(f"{testDefine}")
-        print("頁面成功刷新")
+        print("✅ 頁面成功刷新")
     except TimeoutException:
         print("⚠️ 頁面未成功刷新，停止爬取")
         break
@@ -194,7 +194,7 @@ df = df.groupby(["商品編號"], as_index=False).agg({
     "商品名稱": "first",  # 保留第一個名稱
     "庫存": "sum"  # 合併庫存數量
 })
-
+driver.quit()
 #  === 自定義排序規則 === 
 custom_order = [
     "EBEA0000000", "EBFA0000000", "EBGA0000000", "EBHA0000000", "EBJA0000000", "EBMA0000000",
@@ -265,6 +265,7 @@ print("✅ 數據已寫入 F 欄並更新時間！")
 
 # **關閉瀏覽器**
 driver.quit()
+
 
 
 
