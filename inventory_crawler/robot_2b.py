7e7d1bb875bac5aa609df7b1070bb07a350bae24
配客嘉 <139420301+packageplus@users.noreply.github.com>
2025-08-20 16:34:04 +0800

    Update robot_2b.py

--- inventory_crawler/robot_2b.py
+++ inventory_crawler/robot_2b.py
@@ -162,10 +162,10 @@ while True:
     # 等到表格內容真的換新
     try:
         wait.until(
-            # lambda d: d.find_elements(By.CSS_SELECTOR, "#stock-table tbody tr")
             lambda d: d.find_elements(By.CSS_SELECTOR, ".rdt_TableBody .rdt_TableRow")
-                      # and d.find_elements(By.CSS_SELECTOR, "#stock-table tbody tr")[0].text != first_row_text
-                      and d.find_elements(By.CSS_SELECTOR, ".rdt_TableBody .rdt_TableRow")[0].text != first_row_text
+                      and d.find_elements(By.CSS_SELECTOR, ".rdt_TableBody .rdt_TableRow")[0].text.strip() != first_row_text
+            # lambda d: d.find_elements(By.CSS_SELECTOR, "#stock-table tbody tr")
+                        # and d.find_elements(By.CSS_SELECTOR, "#stock-table tbody tr")[0].text != first_row_text
         )
         print("頁面成功刷新")
     except TimeoutException:
@@ -251,6 +251,7 @@ print("✅ 數據已寫入 F 欄並更新時間！")
 
 # **關閉瀏覽器**
 driver.quit()
+
 
 
 
